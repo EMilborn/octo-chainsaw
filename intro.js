f47a5b515f572d9f9c88ef6ed31ed1616219fdc3
ayu7 <ayu7@stuy.edu>
2016-12-16 12:37:46 -0500

    some stuff

---
+++ intro.js
@@ -0,0 +1,25 @@
+/*Write a javascript file that will do the following:
+
+    When the button is pushed, add an element to the list
+    When the mouse goes over an item in the list, change the heading at the top to contain the text of the item
+    When the mouse is no longer over an item in the list, change the heading back to "Hello World!"
+    When an item on the list is clicked, remove it from the DOM
+
+Add the following to your previous work:
+
+    Add a second list to the html page, do not add elements to it.
+    Create a second button.
+    When the second button is pressed, the new list should have an item added that contains the next fibonacci number.
+    For the mathematically inclined, you could add more lists that generate other numeric sequences if you like.*/
+
+//thelist.addEventListener("mouseover", function() {console.log("bleh")});
+
+var listItem=function(){
+    var x=document.createElement("li");
+    x.innerhtml="new thing"; 
+}
+
+
+b.addEventerListener("click", thelist.appendChild(listItem));
+
+		     
